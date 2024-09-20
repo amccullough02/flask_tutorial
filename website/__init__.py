ac9e711390d17a6210cf982c7296fb3ba22c4ee5
Alex McCullough <alexmccullough@outlook.com>
2024-09-20 12:44:12 +0100

    feat: user and note db models/db init

--- website/__init__.py
+++ website/__init__.py
@@ -1,11 +1,17 @@
 from flask import Flask
 from .views import views
 from .auth import auth
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'asdkwmasdlasdk'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
